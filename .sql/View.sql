f91c6735cf5024fef222dde75fec93b3e00fafab
Mohammad Erfan Khebrati <khebrati@gmail.com>
2024-05-31 16:33:49 +0330

    fixed typo in view file

--- .sql/View.sql
+++ .sql/View.sql
@@ -1,3 +1,3 @@
-create table View(PostID integer references Post, AccountID integer references Account);
+create table Hamsell.View(PostID integer references Post, AccountID integer references Hamsell.Account);
 alter table Hamsell.View add(primary key(PostID,AccountId));
 desc Hamsell.View;
