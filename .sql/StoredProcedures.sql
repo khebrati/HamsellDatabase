2b3080c5cf9ab72ca5ad003b6ae2a3ef8a95e95d
Ali-Borhani <117738373+A-brhn@users.noreply.github.com>
2024-06-04 10:57:46 +0330

    Basic idea of second Procedure

--- .sql/StoredProcedures.sql
+++ .sql/StoredProcedures.sql
@@ -8,4 +8,22 @@ BEGIN
 END //
 DELIMITER ;
 -- call GetPostTitleByEmail('john.doe@example.com');
--- DROP PROCEDURE GetPostTitleByEmail;
+
+
+-- no.2
+DELIMITER //
+CREATE PROCEDURE GetUserByAdminEmail(in EmailAddress VARCHAR(254))
+BEGIN
+	declare var int;
+    declare PostID int;
+    declare UserID int;
+    select AccountId into var from Account where Account.EmailAddress = EmailAddress;
+    select PostID into PostID from PostModeration where PostModeration.AdminId = var;
+    select UserId into UserID from Post where Post.PostID = PostID; 
+    select FirstName, LastName from Account where Account.AccountID = UserID; 
+END //
+DELIMITER ;
+
+
+
+
